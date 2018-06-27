52f786db883f56d6cf246924d201f9392539932f
yihewang1234 <yihe.wang@roardota.com>
2018-06-27 14:23:12 -0400

    init

--- gpyopt_test.py
+++ gpyopt_test.py
@@ -1,23 +1,40 @@
 import GPyOpt
-
+import pandas as pd
 import numpy as np
 from statsmodels.tsa.arima_model import ARIMA
 
-n = 100
-data = [1, 2]
+n = 200
+data = [0, 1]
 np.random.seed(123)
+err = np.random.normal(0, 1, n+2)
 gp_params = {"alpha": 1e-5, "n_restarts_optimizer": 2}
 for i in range(n):
-    data.append(data[-1]*0.8 - data[-2]*0.3 + np.random.normal(0, 0.1, 1)[0])
+    data.append(data[-1]*0.6 - data[-2]*0.3 + 0.7*err[i] - 0.2*err[i+1])
 
-
-def arima(p, d, q):
-    print(p,d,q)
+def arima(params):
+    p, d, q = params[0]
+    p = int(p)
+    d = int(d)
+    q = int(q)
+    #print(p, d, q)
     try:
         model = ARIMA(data[:-10], order=(p, d, q))
         result = model.fit()
         #result.predict(start=n+1, end=n+10)
-        return -result.aic
+        #print(result.aic)
+        return result.aic
     except ValueError:
-        return 100000
+        #print("not use")
+        return np.inf
+
+
+bounds = [{'name': 'p', 'type': 'discrete', 'domain': (1, 8)},
+          {'name': 'd', 'type': 'discrete', 'domain': (0, 4)},
+          {'name': 'q', 'type': 'discrete', 'domain': (1, 8)}]
+
+max_iter = 100
+myProblem = GPyOpt.methods.BayesianOptimization(arima, bounds, maximize=False)
+myProblem.run_optimization(max_iter)
+print(myProblem.x_opt)
+print(myProblem.fx_opt)
 
